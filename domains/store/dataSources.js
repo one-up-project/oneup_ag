baf55a372cfc233073d4cf0794acea68e461384f
MiguelPinzonG <mipinzong02@gmail.com>
2025-03-03 20:37:44 -0500

    Cambio url

--- domains/store/dataSources.js
+++ domains/store/dataSources.js
@@ -3,7 +3,7 @@ import { RESTDataSource } from "@apollo/datasource-rest";
 export class StoreAPI extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = "http://localhost:5000/api/store/"; // URL del backend
+    this.baseURL = "http://localhost:8080/api/store/"; // URL del backend
   }
 
   // GET
